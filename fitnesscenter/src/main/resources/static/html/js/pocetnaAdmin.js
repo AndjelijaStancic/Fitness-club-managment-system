b3aae6e36ad307fdbf977a80ef467f1682e4758c
AndjelijaStancic <andjica.stancic@gmail.com>
2021-06-07 01:21:36 +0200

    ZastitaZaSveHtmlStrane

---
+++ fitnesscenter/src/main/resources/static/html/js/pocetnaAdmin.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    let uloga = localStorage.getItem("uloga");
+    if (uloga == null) {
+        localStorage.setItem("uloga", "null");
+    }
+    if (uloga == "null") {
+        alert("Nemate pristup ovoj stranici!");
+        window.location.href = "../index.html";
+    }
+    if (uloga == "clan") {
+        alert("Nemate pristup ovoj stranici!");
+        window.location.href = "../clan/pocetna.html";
+    }
+    if (uloga == "trener") {
+        alert("Nemate pristup ovoj stranici!");
+        window.location.href = "../trener/pocetna.html";
+    }
+});
---
+++ fitnesscenter/src/main/resources/static/html/js/pocetnaTrener.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    let uloga = localStorage.getItem("uloga");
+    if (uloga == null) {
+        localStorage.setItem("uloga", "null");
+    }
+    if (uloga == "null") {
+        alert("Nemate pristup ovoj stranici!");
+        window.location.href = "../index.html";
+    }
+    if (uloga == "admin") {
+        alert("Nemate pristup ovoj stranici!");
+        window.location.href = "../admin/pocetna.html";
+    }
+    if (uloga == "clan") {
+        alert("Nemate pristup ovoj stranici!");
+        window.location.href = "../clan/pocetna.html";
+    }
+});
